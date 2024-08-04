9dc31858622127de0ac5146f9747ee0b15fb1e50
PrajwalK44 <153058777+PrajwalK44@users.noreply.github.com>
2024-08-04 18:43:06 +0530

    Update Anchor.js

--- Git/Frontend/components/Anchor.js
+++ Git/Frontend/components/Anchor.js
@@ -2,98 +2,114 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import BarChart from './BarChart';
 import { useNavigate } from 'react-router-dom';
-import Barcharthori from './Barcharthori';
+import Navbar from './Navbar';
+import '../css/Anchor.css';
 
 const Anchor = () => {
     const [anchor, setAnchor] = useState([]);
     const [chartData, setChartData] = useState({ labels: [], values: [] });
     const [searchTerm, setSearchTerm] = useState('');
 
-
     useEffect(() => {
         const fetchAnchor = async () => {
             try {
                 const response = await axios.get('http://localhost:4000/anchor/getAnchors');
-                setAnchor(response.data); // Assuming response.data is the entire JSON object
-                
-                // Example assuming response.data has properties like 'anchors' and 'views'
-                const labels = response.data.map(item => item.Anchor); // Adjust according to your JSON structure
-                const values = response.data.map(item => item.Views); // Adjust according to your JSON structure
-    
+                setAnchor(response.data);
+
+                const labels = response.data.map(item => item.Anchor);
+                const values = response.data.map(item => item.Views);
+
                 setChartData({ labels, values });
             } catch (error) {
                 console.error('Error fetching anchor data:', error);
             }
         };
-    
+
         fetchAnchor();
     }, []);
 
-    const Navigate = useNavigate();
+    const navigate = useNavigate();
 
     const openAnchorPage = (anchorName) => {
-        Navigate(`/anchor/${anchorName}`); 
+        navigate(`/anchor/${anchorName}`);
     };
 
     const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value.toLowerCase()); 
-      };
-    
-      const filteredAnchors = anchor.filter((an) => {
-        return (
-          typeof an.Anchor === 'string' &&
-          typeof an.Channel === 'string' &&
-          (
-            an.Anchor.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            an.Channel.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-        );
-      });
+        setSearchTerm(event.target.value.toLowerCase());
+    };
 
-      
-      
-    
-    
-    
+    const filteredAnchors = anchor.filter((an) => {
+        return (
+            typeof an.Anchor === 'string' &&
+            typeof an.Channel === 'string' &&
+            (
+                an.Anchor.toLowerCase().includes(searchTerm) ||
+                an.Channel.toLowerCase().includes(searchTerm)
+            )
+        );
+    });
 
     return (
         <>
-        <center><p class="h1 mb-3">Anchor Analysis</p>
-
-        <input class="mb-3" type="text" placeholder="Search...." value={searchTerm} onChange={handleSearchChange}/> </center>
-        <div style={{ maxHeight: '400px', overflowY: 'scroll' }}>
-         <table class="table table-dark table-striped">
-            
-            <thead style={{ position: 'sticky', top: '0', zIndex: '1', background: '#343a40'}}>
-                <tr>
-                <th scope="col"></th>
-                <th scope="col">Anchor</th>
-                <th scope="col">Channel</th>
-                <th scope="col">Views</th>
-                {/* <th scope="col">Sentiment</th> */}
-                <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                {filteredAnchors.map((item, index) => (
-                            <tr key={index}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{item.Anchor}</td> 
-                                <td>{item.Channel}</td> 
-                                <td>{item.Views}</td> 
-                                {/* <td><Barcharthori Positive={item.Positive} Negative={ item.Negative}></Barcharthori></td> */}
-                                <td><button type="button" class="btn btn-outline-info" onClick={() => openAnchorPage(item.Anchor)}>Info</button></td> 
-                            </tr>
-                        ))}
-            </tbody>
-            </table>
+            <Navbar />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="centered-container">
+                <p className="h1">Anchor Analysis</p>
+                <div className="search-container">
+                    <input
+                        className="search-bar"
+                        type="text"
+                        placeholder="Search...."
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
+                </div>
             </div>
-            <center>
-                <p class="h1">Anchor Analysis based on Views</p>
-                <BarChart data={chartData} label1="Views" />
-            </center>
-            </>
+            <div style={{ maxHeight: '400px', overflowY: 'scroll', width: '100%' }}>
+            <table className="table table-light table-striped table-light-custom">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Anchor <i className="fa-solid fa-user"></i></th>
+                        <th scope="col">Channel <i className="fa-solid fa-tv"></i></th>
+                        <th scope="col">Views <i className="fa-solid fa-eye"></i></th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredAnchors.map((item, index) => (
+                        <tr key={index}>
+                            <th scope="row">{index + 1}</th>
+                            <td>{item.Anchor}</td>
+                            <td>{item.Channel}</td>
+                            <td>{item.Views}</td>
+                            <td>
+                                <button 
+                                    type="button" 
+                                    className="btn btn-outline-info" 
+                                    onClick={() => openAnchorPage(item.Anchor)}
+                                    style={{ borderColor: '#343a40', color: '#211e1e' }}
+                                >
+                                    Info <i className="fa-solid fa-right"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+            <div className="centered-container">
+    <p className="h1">Anchor Analysis based on Views</p>
+    <div className="chart-container-anchor">
+        <BarChart data={chartData} label1="Views" />
+    </div>
+</div>
+
+        </>
     );
-  }
-  
-  export default Anchor;
+}
+
+export default Anchor;
